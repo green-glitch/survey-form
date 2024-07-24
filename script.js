10892ed8a82dc6e9f072c85f07fdf494cb0209db
haskcoder <kalinowskik234@gmail.com>
2024-07-24 15:50:29 +0200

    4th commit

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const fullname = document.getElementById("name");
 const email = document.getElementById("email");
 const number = document.getElementById("number");
 const dropdown = document.getElementById("dropdown");
+const options = document.getElementsByTagName("option");
 const recommend = document.getElementsByName("recommend");
 const about = document.getElementsByName("about");
-
 
 function SubmitForm(){
     let fn = fullname.value;
@@ -13,17 +13,18 @@ function SubmitForm(){
     let drop = dropdown.value;
 
     if(fn == "" || em == "" || age == ""){
-        alert("Please fill all the fields");
+        //alert("Please fill all the fields");
     }
     else{
         message = [
-            "# Inputs: \n\n",
+            "Inputs: \n\n",
             "Name: " + fn + "\n",
             "Email: " + em + "\n",
             "Age: " + age + "\n",
-            "Satisfied? " + drop + "\n",
+            "Satisfied? " + options[parseInt(drop) + 3].innerText + "\n",
         ];
 
-        alert(message.join(""));
+        // alert(message.join(""));
     }
+    alert("The form will be working soon.");
 }
