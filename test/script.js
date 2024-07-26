903cdd8d037166f2a2a57f74d833544d5fd8d65e
haskcoder <kalinowskik234@gmail.com>
2024-07-26 19:09:15 +0200

    'checked' test

--- test/script.js
+++ test/script.js
@@ -1,3 +1,9 @@
-function GetRecommend(){
+let recommend = document.getElementsByName("recommend");
 
+function GetRecommend(){
+    for (let i = 0; i <= 4; i++){
+        alert(i + "" + recommend[i].checked);
+    }
 }
+
+GetRecommend();
